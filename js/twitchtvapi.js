b808ba56a7f007bb9faa3fbf20e83e69bf3e2f7b
koop4 <dilillo.roberto@gmail.com>
2018-05-27 14:32:07 +0200

    stramer card style

---
+++ js/twitchtvapi.js
@@ -0,0 +1,17 @@
+$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+  console.log(data);
+});
+
+
+
+/*
+freecodecamp
+scarlettm
+dendi
+admiralbulldog
+sing_sing
+thevalentinanappi
+
+https://wind-bow.glitch.me/twitch-api/users/ */
+
+
